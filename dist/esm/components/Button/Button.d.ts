18528ba0b56e0f783abb813628fab789b8658d5a
Alex <33497058+bexsoft@users.noreply.github.com>
2022-09-02 17:35:43 -0500

    Release v0.0.1-alpha (#55)
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

---
+++ dist/esm/components/Button/Button.d.ts
@@ -0,0 +1,4 @@
+import { FC } from "react";
+import { ButtonProps } from "./Button.types";
+declare const Button: FC<ButtonProps>;
+export default Button;
---
+++ dist/esm/utils/TestIcon.d.ts
@@ -0,0 +1,3 @@
+import { SVGProps } from "react";
+declare const TestIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+export default TestIcon;
