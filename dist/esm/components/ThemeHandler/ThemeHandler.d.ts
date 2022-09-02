92822e245761bcb49fb4bb71e6db2d2b5cc8646e
Benjamin Perez <benjamin@bexsoft.net>
2022-09-01 22:51:33 -0500

    Release v0.0.1-alpha
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

---
+++ dist/esm/components/ThemeHandler/ThemeHandler.d.ts
@@ -0,0 +1,4 @@
+import { FC } from "react";
+import { ThemeHandlerProps } from "./ThemeHandler.types";
+declare const ThemeHandler: FC<ThemeHandlerProps>;
+export default ThemeHandler;
---
+++ dist/esm/global/themes.d.ts
@@ -0,0 +1,196 @@
+export declare const lightColors: {
+    white: string;
+    mainGrey: string;
+    disabledGrey: string;
+    hoverGrey: string;
+    pressedGrey: string;
+    actionDisabledGrey: string;
+    mainBlue: string;
+    hoverBlue: string;
+    pressedBlue: string;
+    mainRed: string;
+    hoverRed: string;
+    lightRed: string;
+    divisorColor: string;
+};
+export declare const darkColors: {
+    dark: string;
+    mainGrey: string;
+    disabledGrey: string;
+    hoverGrey: string;
+    borderPressedGrey: string;
+    pressedGrey: string;
+    mainWhite: string;
+    disabledWhite: string;
+    hoverWhite: string;
+    pressedWhite: string;
+    mainRed: string;
+    hoverRed: string;
+    divisorColor: string;
+};
+export declare const lightTheme: {
+    bgColor: string;
+    buttons: {
+        regular: {
+            enabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            disabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            hover: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            pressed: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+        };
+        callAction: {
+            enabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            disabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            hover: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            pressed: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+        };
+        secondary: {
+            enabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            disabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            hover: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            pressed: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+        };
+    };
+};
+export declare const darkTheme: {
+    bgColor: string;
+    buttons: {
+        regular: {
+            enabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            disabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            hover: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            pressed: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+        };
+        callAction: {
+            enabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            disabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            hover: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            pressed: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+        };
+        secondary: {
+            enabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            disabled: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            hover: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+            pressed: {
+                border: string;
+                text: string;
+                background: string;
+                iconColor: string;
+            };
+        };
+    };
+};
