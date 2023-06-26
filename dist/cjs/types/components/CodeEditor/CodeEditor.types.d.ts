3646c870159b6f16cba5d4ece55148eb1f73b131
Alex <33497058+bexsoft@users.noreply.github.com>
2023-06-26 12:15:25 -0600

    Release v0.6.6 (#389)
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

---
+++ dist/cjs/types/components/CodeEditor/CodeEditor.types.d.ts
@@ -0,0 +1,18 @@
+import React from "react";
+import { CSSObject } from "styled-components";
+export interface CodeEditorProps {
+  value: string;
+  label?: string;
+  mode?: string;
+  tooltip?: string;
+  editorHeight?: string | number;
+  onChange: (value: string) => any;
+  className?: string;
+  helpTools?: React.ReactNode;
+  sx?: CSSObject;
+}
+export interface CodeEditorBaseProps {
+  editorHeight: string | number;
+  sx?: CSSObject;
+  className?: string;
+}
