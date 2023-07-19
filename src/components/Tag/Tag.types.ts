c9d1e562f6f0e1707766d87895029511191292b9
Alex <33497058+bexsoft@users.noreply.github.com>
2023-07-19 11:26:55 -0600

    Added new Outlined & Icon Tag styles (#420)
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

--- src/components/Tag/Tag.types.ts
+++ src/components/Tag/Tag.types.ts
@@ -14,17 +14,20 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import { ReactNode } from "react";
 import { CSSObject } from "styled-components";
 
 export interface TagMainProps {
   label: string;
   onDelete?: (item: string) => void;
   id: string;
+  icon?: ReactNode;
 }
 
 export interface TagConstructProps {
   color?: "default" | "secondary" | "warn" | "alert" | "ok";
   sx?: CSSObject;
+  variant?: "regular" | "outlined";
 }
 
 export type TagProps = TagMainProps & TagConstructProps;
