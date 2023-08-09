f204b55a6db925f72150969d57b3ecc1b4539106
Benjamin Perez <benjamin@bexsoft.net>
2023-08-08 18:38:13 -0600

    Added ProgressBar component
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,134 @@
+// This file is part of MinIO Design System
+// Copyright (c) 2023 MinIO, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import React, { FC } from "react";
+import styled, { keyframes } from "styled-components";
+import get from "lodash/get";
+import { CommonProgressBar, ProgressBarProps } from "./ProgressBar.types";
+import { lightColors } from "../../global/themes";
+import Box from "../Box/Box";
+
+const colorItems = {
+  blue: "main",
+  red: "danger",
+  green: "good",
+  orange: "warning",
+  grey: "disabled",
+};
+
+const ProgressBase = styled.div<CommonProgressBar>(({ theme, sx, color }) => ({
+  "& .progBlock": {
+    display: "flex",
+    alignItems: "center",
+    gap: 10,
+  },
+  "& .progressContainer": {
+    position: "relative",
+    width: "100%",
+    height: 8,
+    backgroundColor: get(theme, "boxBackground", lightColors.boxBackground),
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  "& .notificationLabel": {
+    fontSize: 12,
+    color: get(
+      theme,
+      `signalColors.${colorItems[color || "blue"]}`,
+      lightColors.mainBlue,
+    ),
+  },
+  "& .percentageBar": {
+    display: "block",
+    height: 8,
+    backgroundColor: get(
+      theme,
+      `signalColors.${colorItems[color || "blue"]}`,
+      lightColors.mainBlue,
+    ),
+    transitionDuration: "0.1s",
+    borderRadius: 8,
+  },
+  ...sx,
+}));
+
+export const innerAnimation = keyframes`0% {
+                                          left: -100px;
+                                          visibility: visible;
+                                          width: 100px;
+                                        }
+                                          60% {
+                                            width: 300px
+                                          }
+                                          99% {
+                                            left: calc(100% + 300px);
+                                          }
+                                          100% {
+                                            visibility: hidden;
+                                            width: 100px;
+                                          }`;
+
+const IndeterminateItem = styled.div<CommonProgressBar>`
+  width: 100px;
+  height: 8px;
+  display: block;
+  position: absolute;
+  border-radius: 8px;
+  animation: ${innerAnimation} 1000ms linear infinite normal forwards;
+  background-color: ${(props) =>
+    get(
+      props.theme,
+      `signalColors.${colorItems[props.color || "blue"]}`,
+      lightColors.mainBlue,
+    )};
+`;
+
+const ProgressBar: FC<ProgressBarProps> = ({
+  progressLabel = false,
+  sx,
+  value = 0,
+  maxValue = 100,
+  variant = "indeterminate",
+  notificationLabel = "",
+  color = "blue",
+}) => {
+  const calcPerc = (value * 100) / maxValue;
+
+  return (
+    <ProgressBase color={color} sx={sx}>
+      <Box className={"progBlock"}>
+        <Box className={"progressContainer"}>
+          {variant === "indeterminate" ? (
+            <IndeterminateItem color={color} />
+          ) : (
+            <Box
+              className={"percentageBar"}
+              style={{ width: `${calcPerc}%` }}
+            />
+          )}
+        </Box>
+        {progressLabel && variant !== "indeterminate" && (
+          <span className={"progressPercentage"}>{Math.floor(calcPerc)}%</span>
+        )}
+      </Box>
+      {notificationLabel !== "" && (
+        <span className={"notificationLabel"}>{notificationLabel}</span>
+      )}
+    </ProgressBase>
+  );
+};
+
+export default ProgressBar;
