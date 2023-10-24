37534c8211df37d7d5216411d3ad568e4e9d6162
Benjamin Perez <benjamin@bexsoft.net>
2023-10-23 19:07:35 -0600

    Updated Storybook & dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,9 @@ module.exports = {
     "../src/**/**/*.stories.mdx",
     "../src/**/**/*.stories.@(js|jsx|ts|tsx)",
   ],
+  core: {
+    disableTelemetry: true,
+  },
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
