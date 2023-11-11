becedbe19482ca69e53b4082a2290d06a39563ac
Alex <33497058+bexsoft@users.noreply.github.com>
2023-11-10 22:10:49 -0600

    Release v0.12.0 (#572)
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

---
+++ dist/cjs/types/components/DateTimeInput/DateTimeSelector.d.ts
@@ -0,0 +1,4 @@
+import { FC } from "react";
+import { DateTimeSelectorProps } from "./DateTimeInput.types";
+declare const DateTimeSelector: FC<DateTimeSelectorProps>;
+export default DateTimeSelector;
---
+++ dist/esm/types/components/DateTimeInput/DateTimeInput.types.d.ts
@@ -0,0 +1,58 @@
+import React from "react";
+import { DateTime } from "luxon";
+import { CSSObject } from "styled-components";
+import { CommonHelpTipPlacement } from "../HelpTip/HelpTip.types";
+export interface DateTimeInputMain {
+  pickerStartComponent?: React.ReactNode;
+  className?: string;
+  label?: string;
+  required?: boolean;
+  tooltip?: string;
+  name: string;
+  disabled?: boolean;
+  openPickerIcon?: "arrow" | React.ReactNode;
+  displayFormat?: string;
+  helpTip?: React.ReactNode;
+  helpTipPlacement?: CommonHelpTipPlacement;
+  noLabelMinWidth?: boolean;
+  pickerSx?: CSSObject;
+}
+export interface DateTimeConstruct {
+  id: string;
+  sx?: CSSObject;
+  mode?: "all" | "date";
+  value: DateTime | null;
+  onChange: (value: DateTime | null) => void;
+  minDate?: DateTime;
+  maxDate?: DateTime;
+  usePortal?: boolean;
+}
+export interface DateTimeSelectorMain {
+  open?: boolean;
+  anchorEl?: (EventTarget & HTMLElement) | null;
+  onClose?: () => void;
+}
+export interface TimeSelectorProps {
+  value: DateTime | null;
+  onChange: (value: DateTime | null) => void;
+  completeCallback?: () => void;
+  timeFormat: "12h" | "24h";
+  secondsSelector: boolean;
+}
+export interface DateSelectorProps {
+  minDate?: DateTime;
+  maxDate?: DateTime;
+  value: DateTime | null;
+  onChange: (value: DateTime | null) => void;
+}
+export interface StylesOverrideProps {
+  isPortal: boolean;
+  mode: "all" | "date";
+  sx?: CSSObject;
+}
+export type DateTimeInputProps = DateTimeInputMain &
+  DateTimeConstruct &
+  TimeSelectorProps;
+export type DateTimeSelectorProps = DateTimeSelectorMain &
+  DateTimeConstruct &
+  TimeSelectorProps;
