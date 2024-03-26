8c39b07f308b91e669ecbf67922c8b0132995233
Benjamin Perez <benjamin@bexsoft.net>
2024-03-25 18:00:52 -0600

    Updated project dependencies
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { Meta, Story } from "@storybook/react";
 
 import Select from "./Select";
