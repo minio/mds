67de589d9eaac84403ab79817db18a87c79966b0
Benjamin Perez <benjamin@bexsoft.net>
2024-04-28 22:00:42 -0600

    Release v2.1.5
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

---
+++ dist/esm/types/components/Icons/NewDesignIcons/ChevronRightIcon.d.ts
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { SVGProps } from "react";
+declare const ChevronRightIcon: (
+  props: SVGProps<SVGSVGElement>,
+) => React.JSX.Element;
+export default ChevronRightIcon;
