ed941e0ccf7c282c11266b0b0ada30bd5e33ec77
Benjamin Perez <benjamin@bexsoft.net>
2024-07-23 12:05:05 -0600

    Updated border-box sizing for overlay object
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

--- src/components/InputBox/InputBox.tsx
+++ src/components/InputBox/InputBox.tsx
@@ -146,6 +146,7 @@ const InputContainer = styled.div<InputContainerProps>(
         borderLeft: `1px solid ${theme.colors["Color/Neutral/Border/colorBorderSubtle"]}`,
         boxShadow: "none",
         height: sizeMode === "small" ? 26 : 36,
+        boxSizing: "border-box",
         "& .min-icon": {
           width: 16,
           height: 16,
