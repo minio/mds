60e17fca754b0a436f1f418c5d5c53fd308052ce
Victor Bayas <victorsbayas@gmail.com>
2024-08-22 20:25:41 -0500

    Release v2.3.19

--- dist/esm/types/src/components/DataTable/DataTable.d.ts
+++ dist/esm/types/src/components/DataTable/DataTable.d.ts
@@ -1,4 +1,30 @@
-import { FC } from "react";
+import React from "react";
 import { DataTableProps } from "./DataTable.types";
-declare const DataTable: FC<DataTableProps>;
+declare const DataTable: <T>({
+  itemActions,
+  columns,
+  onSelect,
+  records,
+  isLoading,
+  loadingMessage,
+  entityName,
+  selectedItems,
+  idField,
+  customEmptyMessage,
+  customPaperHeight,
+  columnsSelector,
+  textSelectable,
+  columnsShown,
+  onColumnChange,
+  infiniteScrollConfig,
+  autoScrollToBottom,
+  disabled,
+  onSelectAll,
+  rowStyle,
+  parentClassName,
+  sx,
+  rowHeight,
+  sortEnabled,
+  sortCallBack,
+}: DataTableProps<T>) => React.JSX.Element;
 export default DataTable;
--- dist/esm/types/src/components/DataTable/DataTable.utils.d.ts
+++ dist/esm/types/src/components/DataTable/DataTable.utils.d.ts
@@ -2,22 +2,22 @@ import React from "react";
 import { SortDirectionType } from "react-virtualized";
 import { IColumns, ISortConfig, ItemActions } from "./DataTable.types";
 export declare const selectWidth = 45;
-export declare const generateColumnsMap: (
-  columns: IColumns[],
+export declare const generateColumnsMap: <T>(
+  columns: IColumns<T>[],
   containerWidth: number,
   actionsWidth: number,
   hasSelect: boolean,
   hasActions: boolean,
-  selectedItems: string[],
-  idField: string,
+  selectedItems: Array<keyof T> | string[],
+  idField: keyof T,
   columnsSelector: boolean,
-  columnsShown: string[],
-  sortColumns: boolean | string[] | ISortConfig,
-  currentSortColumn: string | undefined,
+  columnsShown: Array<keyof T>,
+  sortColumns: boolean | Array<keyof T> | ISortConfig,
+  currentSortColumn: keyof T | undefined,
   currentSortDirection: "ASC" | "DESC" | undefined,
 ) => (React.JSX.Element | null)[];
-export declare const elementActions: (
-  actions: ItemActions[],
+export declare const elementActions: <T>(
+  actions: ItemActions<T>[],
   valueToSend: any,
   selected: boolean,
   idField: string,
