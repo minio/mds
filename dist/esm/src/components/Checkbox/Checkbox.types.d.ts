bfaf273f7f806878d4affadda75d4598ebffd2d3
Benjamin Perez <benjamin@bexsoft.net>
2024-11-01 17:31:33 -0600

    Release v3.1.2
    Signed-off-by: Benjamin Perez <benjamin@bexsoft.net>

--- dist/esm/src/components/Checkbox/Checkbox.types.d.ts
+++ dist/esm/src/components/Checkbox/Checkbox.types.d.ts
@@ -4,6 +4,7 @@ export interface CheckboxProps extends HTMLAttributes<HTMLInputElement> {
   label?: string;
   tooltip?: string;
   overrideLabelClasses?: string;
+  noLabelMinWidth?: boolean;
   sx?: OverrideTheme;
   helper?: string;
 }
